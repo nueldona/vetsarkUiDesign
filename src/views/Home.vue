95cf4a97e06b50cde28a9a794bd7cf266050f5a6
Babatunde Omonijo <SONY@IT-BABATUNDE-OMONIJO.CHQ.NNPCGroup.local>
2021-05-17 14:54:02 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="grid grid-cols-2">
+    <Hero class="" />
+    <Form />
+  </div>
+</template>
+
+<script>
+import Hero from "@/components/Layout/Hero.vue";
+import Form from "@/components/Layout/Form.vue";
+export default {
+  components: {
+    Hero,
+    Form
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: '#e53e3e',
+      },
+      height: {
+        "400px": "400px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
